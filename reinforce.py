5197e98249d29d706b60cfe5aedd5a4f2b43e6e3
chippybautista <chippybautista@gmail.com>
2019-05-08 08:45:34 +0800

    fix reinforce validation bug

--- reinforce.py
+++ reinforce.py
@@ -10,13 +10,6 @@ from settings import *
 from data import MSCOCO
 
 
-def captions_to_coco_format(img_ids, captions):
-    return {
-        img_id: [cap]
-        for img_id, cap in zip(img_ids, captions)
-    }
-
-
 RUN_IDENTIFIER = time.strftime('RL-%m%d-%H%M-E')
 env = Environment()
 agent = Agent(LEARNING_RATE_RL)
@@ -25,17 +18,18 @@ agent.actor.load_state_dict(torch.load(
     MODEL_WEIGHTS, map_location=None if USE_CUDA else 'cpu'
 )['model_state_dict'])
 
-train_loader = DataLoader(MSCOCO('train', evaluation=True),
+train_loader = DataLoader(MSCOCO('val', evaluation=True),
                           batch_size=BATCH_SIZE_RL, shuffle=SHUFFLE)
 val_loader = DataLoader(MSCOCO('val', evaluation=True),
-                        batch_size=BATCH_SIZE_RL, shuffle=SHUFFLE)
+                        shuffle=SHUFFLE)
 
-min_val_reward = 0.0
+max_val_reward = 0.0
 for e in range(10):
     epoch_start = time.time()
 
     # TRAINING
     rewards = []
+    g_rewards = []
     for img_ids, img_features, captions in train_loader:
         agent.actor_optim.zero_grad()
 
@@ -44,20 +38,20 @@ for e in range(10):
         # use .unsqueeze() to make the original shape (5, 1, 36, 2048)
         img_features = img_features.unsqueeze(1)
 
-        sampled_captions = []
-        greedy_captions = []
+        sampled_captions = {}
+        greedy_captions = {}
         sampled_log_probs = []
-        for img_feat in img_features:  # each iteration is a single image
+        for img_id, img_feat in zip(img_ids, img_features):
             _, init_state, init_lstm_states = env.reset(img_feat)
             sampled_caption, probs = agent.inference(
                 init_state, init_lstm_states, env)
+            sampled_captions[img_id] = [sampled_caption]
+            sampled_log_probs.append(probs)
+
             with torch.no_grad():
                 greedy_caption, _ = agent.inference(
                     init_state, init_lstm_states, env, 'greedy')
-
-            sampled_log_probs.append(probs)
-            sampled_captions.append(sampled_caption)
-            greedy_captions.append(greedy_caption)
+                greedy_captions[img_id] = [greedy_caption]
 
         # pytorch's dataloader does something to the lists,
         # but i dont need it and i dont know how to turn it off.
@@ -66,16 +60,10 @@ for e in range(10):
 
         # transform ground truth and results to the format needed for eval
         ground_truth = dict(zip(img_ids, map(list, captions)))
-        sampled_captions = captions_to_coco_format(
-            img_ids, sampled_captions)
-        greedy_captions = captions_to_coco_format(
-            img_ids, greedy_captions)
 
         # calculate CIDEr scores
-        mean_sample_scores, sample_scores = env.get_cider_score(
-            ground_truth, sampled_captions)
-        mean_greedy_scores, greedy_scores = env.get_cider_score(
-            ground_truth, greedy_captions)
+        _, sample_scores = env.get_cider_score(ground_truth, sampled_captions)
+        _, greedy_scores = env.get_cider_score(ground_truth, greedy_captions)
 
         # self-critical: score from sampling - score from test time algo
         advantages = torch.Tensor((sample_scores - greedy_scores).reshape(-1))
@@ -85,38 +73,35 @@ for e in range(10):
         loss = -(advantages * torch.stack(sampled_log_probs)).mean()
         loss.backward()
         agent.actor_optim.step()
-        rewards.append(mean_sample_scores)
+
+        rewards.extend(sample_scores)
+        g_rewards.extend(greedy_scores)
 
     # VALIDATION
-    rewards_val = []
     with torch.no_grad():
-        for img_ids, img_features, captions in val_loader:
-            img_features = img_features.unsqueeze(1)
-            greedy_captions = []
-            for img_feat in img_features:
-                greedy_caption, _ = agent.inference(
-                    init_state, init_lstm_states, env, 'greedy')
-                greedy_captions.append(greedy_caption)
+        val_ground_truths = {}
+        greedy_captions = {}
+        for img_ids, img_features, captions in val_loader:  # per sample
+            _, init_state, init_lstm_states = env.reset(img_features)
+            greedy_caption, _ = agent.inference(
+                init_state, init_lstm_states, env, 'greedy')
 
-            captions = np.array(captions).T
-            ground_truth = dict(zip(img_ids, map(list, captions)))
-            greedy_captions = captions_to_coco_format(
-                img_ids, greedy_captions)
-            mean_greedy_scores, _ = env.get_cider_score(
-                ground_truth, greedy_captions)
-            rewards_val.append(mean_greedy_scores)
+            val_ground_truths[img_ids[0]] = list(np.array(captions).T[0])
+            greedy_captions[img_ids[0]] = [greedy_caption]
 
-    val_reward = np.mean(rewards_val)
+        _, val_greedy_scores = env.get_cider_score(
+            val_ground_truths, greedy_captions)
+        val_reward = np.mean(val_greedy_scores)
 
     # print('[B] Loss: {:.2f}. Mean reward: {:.2f}. Mean advantage: {:.2f}'.format(
     #     loss.item(), mean_sample_scores, advantages.mean()))
 
-    print('Epoch {}. R train: {:.2f} R val: {:.2f}. {:.2f}'.format(
-        e, np.mean(rewards), val_reward,
+    print('Epoch {} - R train: {:.2f} - R train (greedy): {:.2f} - R val: {:.2f} ({:.2f}s)'.format(
+        e, np.mean(rewards), np.mean(g_rewards), val_reward,
         time.time() - epoch_start))
 
-    if val_reward > min_val_reward:
-        min_val_reward = val_reward
+    if val_reward > max_val_reward:
+        max_val_reward = val_reward
         print('Higher mean validation reward achieved. Saving model.')
         torch.save({
             'epoch': e,
